c159cb0f3494137a025f4641830f2bdcac90a7bc
namdaeun <nde40345@gmail.com>
2024-05-14 18:00:47 +0900

    Feat: 필요한 svg 가져오기

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as IcHamburger } from "./ic_hamburger.svg?react";
+export { default as IcLogo } from "./ic_logo_svg.svg?react";
+export { default as IcMy } from "./ic_my.svg?react";
+export { default as IcSearch } from "./ic_search.svg?react";
