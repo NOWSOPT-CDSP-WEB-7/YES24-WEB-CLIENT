8c2e98fc1fab422557c3f1d906be91d61d8e178f
imhwarang <imhwarang4@naver.com>
2024-05-15 19:29:14 +0900

    Feat: SearchHeader 구현 완료

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Search from "./pages/Search/Search";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" />
-        <Route path="/search" />
+        <Route path="/search" element={<Search />} />
         <Route path="/search/list" />
         <Route path="/detail" />
       </Routes>
