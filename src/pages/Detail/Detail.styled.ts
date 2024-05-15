a302a0f74fbe8589b82b4ca3ee48fa25c4dc71fc
namdaeun <nde40345@gmail.com>
2024-05-16 02:06:20 +0900

    Feat: 공연 디테일 UI 구현

--- src/pages/Detail/Detail.styled.ts
+++ src/pages/Detail/Detail.styled.ts
@@ -1,27 +1,39 @@
 import styled from "styled-components";
 
-export const DetailWrapper = styled.section``;
-
 export const ShowLayout = styled.section`
   padding: 1rem;
 `;
 
 export const Title = styled.h1`
-  padding: 0 1rem;
   ${({ theme }) => theme.fonts.title_18pt_Bold};
 `;
 
 export const ShowEntireInfoLayout = styled.section`
   display: flex;
   gap: 1rem;
-  padding: 1rem;
+  padding: 1rem 0;
 `;
 
-export const ShowImage = styled.img``;
+export const ShowContainer = styled.section`
+  display: flex;
+  gap: 1rem;
+  padding-top: 2rem;
+`;
 
 export const ShowInfoContainer = styled.section`
   display: flex;
   gap: 1rem;
+`;
+
+export const InfoWithBtnLayout = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+`;
+
+export const ShowImage = styled.img`
+  padding-bottom: 1.4rem;
 `;
 
 export const CategoryBox = styled.div`
@@ -47,4 +59,25 @@ export const Info = styled.span`
 export const Place = styled.span`
   color: ${({ theme }) => theme.colors.Primary_orange};
   ${({ theme }) => theme.fonts.sub_12pt};
+`;
+
+export const BtnLayout = styled.div`
+  display: flex;
+  gap: 0.8rem;
+  justify-content: flex-end;
+`;
+
+export const HeartBtnBox = styled.button`
+  display: flex;
+  gap: 0.2rem;
+  height: 2.4rem;
+  padding: 0.5rem 1rem;
+
+  border: 1px solid ${({ theme }) => theme.colors.Gray_stroke};
+  border-radius: 2px;
+`;
+
+export const HeartNum = styled.span`
+  color: ${({ theme }) => theme.colors.Text_01};
+  ${({ theme }) => theme.fonts.sub_12pt_extraBold};
 `;
--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,37 +1,49 @@
+import { IcBtnShare, IcHeartFill } from "../../assets/icons";
+import Header from "../../components/commons/Header/Header";
 import { SHOW_DETAIL } from "../../constants/showDetail";
 import { categories } from "../../constants/showDetailCategory";
 import * as S from "./Detail.styled";
 
 const Detail = () => {
   return (
-    <S.DetailWrapper>
+    <>
+      <Header />
       {SHOW_DETAIL.data.map(
         ({ runShowID, genre, title, period, filmRating, location, place, image, runTime }) => (
           <S.ShowLayout key={runShowID}>
             <S.Title>
               [{location}] {title}
             </S.Title>
-            <S.ShowEntireInfoLayout>
+            <S.ShowContainer>
               <S.ShowImage src={image} />
-              <S.ShowInfoContainer>
-                <S.CategoryBox>
-                  {categories.map((category) => (
-                    <S.Category>{category}</S.Category>
-                  ))}
-                </S.CategoryBox>
-                <S.InfoBox>
-                  <S.Info>{genre}</S.Info>
-                  <S.Info>{period}</S.Info>
-                  <S.Place>{place}</S.Place>
-                  <S.Info>{filmRating}세 이상 관람가</S.Info>
-                  <S.Info>{runTime}분 (인터미션: 20분 포함)</S.Info>
-                </S.InfoBox>
-              </S.ShowInfoContainer>
-            </S.ShowEntireInfoLayout>
+              <S.InfoWithBtnLayout>
+                <S.ShowInfoContainer>
+                  <S.CategoryBox>
+                    {categories.map((category) => (
+                      <S.Category>{category}</S.Category>
+                    ))}
+                  </S.CategoryBox>
+                  <S.InfoBox>
+                    <S.Info>{genre}</S.Info>
+                    <S.Info>{period}</S.Info>
+                    <S.Place>{place}</S.Place>
+                    <S.Info>{filmRating}세 이상 관람가</S.Info>
+                    <S.Info>{runTime}분 (인터미션: 20분 포함)</S.Info>
+                  </S.InfoBox>
+                </S.ShowInfoContainer>
+                <S.BtnLayout>
+                  <S.HeartBtnBox>
+                    <IcHeartFill />
+                    <S.HeartNum>48</S.HeartNum>
+                  </S.HeartBtnBox>
+                  <IcBtnShare />
+                </S.BtnLayout>
+              </S.InfoWithBtnLayout>
+            </S.ShowContainer>
           </S.ShowLayout>
         )
       )}
-    </S.DetailWrapper>
+    </>
   );
 };
 
