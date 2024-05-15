6decd836c6d611b0b8901efd1f7420bb5ad2f01e
namdaeun <nde40345@gmail.com>
2024-05-16 03:31:16 +0900

    Refactor: 컴포넌트 분리 및 스타일 컴포넌트명 수정

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,49 +1,12 @@
-import { IcBtnShare, IcHeartFill } from "../../assets/icons";
 import Header from "../../components/commons/Header/Header";
-import { SHOW_DETAIL } from "../../constants/\bshowDetailData";
 
-import { categories } from "../../constants/showDetailCategory";
-import * as S from "./Detail.styled";
+import ShowInfo from "./components/ShowInfo";
 
 const Detail = () => {
   return (
     <>
       <Header />
-      {SHOW_DETAIL.data.map(
-        ({ runShowID, genre, title, period, filmRating, location, place, image, runtime }) => (
-          <S.ShowLayout key={runShowID}>
-            <S.Title>
-              [{location}] {title}
-            </S.Title>
-            <S.ShowContainer>
-              <S.ShowImage src={image} />
-              <S.InfoWithBtnLayout>
-                <S.ShowInfoContainer>
-                  <S.CategoryBox>
-                    {categories.map((category) => (
-                      <S.Category>{category}</S.Category>
-                    ))}
-                  </S.CategoryBox>
-                  <S.InfoBox>
-                    <S.Info>{genre}</S.Info>
-                    <S.Info>{period}</S.Info>
-                    <S.Place>{place}</S.Place>
-                    <S.Info>{filmRating}세 이상 관람가</S.Info>
-                    <S.Info>{runtime}분 (인터미션: 20분 포함)</S.Info>
-                  </S.InfoBox>
-                </S.ShowInfoContainer>
-                <S.BtnLayout>
-                  <S.HeartBtnBox>
-                    <IcHeartFill />
-                    <S.HeartNum>48</S.HeartNum>
-                  </S.HeartBtnBox>
-                  <IcBtnShare />
-                </S.BtnLayout>
-              </S.InfoWithBtnLayout>
-            </S.ShowContainer>
-          </S.ShowLayout>
-        )
-      )}
+      <ShowInfo />
     </>
   );
 };
