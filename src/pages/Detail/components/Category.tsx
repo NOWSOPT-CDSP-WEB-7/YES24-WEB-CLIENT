7fcc33cb7ae4aa0f623861ddbb7a27d01825921e
namdaeun <nde40345@gmail.com>
2024-05-16 22:16:54 +0900

    Feat: 선택된 카테고리만 스타일 변화 생김

--- src/pages/Detail/components/Category.tsx
+++ src/pages/Detail/components/Category.tsx
@@ -2,14 +2,38 @@ import { useState } from "react";
 import * as S from "./Category.styled";
 
 const Category = () => {
-  const [isSelected, setIsSelected] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("showInfo");
+
+  const handleCategoryClick = (category: string) => {
+    setSelectedCategory(category);
+  };
 
   return (
     <S.CategoryWrapper>
-      <S.Category $isSelected={isSelected}>공연정보</S.Category>
-      <S.Category $isSelected={isSelected}>기대평</S.Category>
-      <S.Category $isSelected={isSelected}>공연 이미지</S.Category>
-      <S.Category $isSelected={isSelected}>상품정보</S.Category>
+      <S.Category
+        $isSelected={selectedCategory === "showInfo"}
+        onClick={() => handleCategoryClick("showInfo")}
+      >
+        공연정보
+      </S.Category>
+      <S.Category
+        $isSelected={selectedCategory === "expectComment"}
+        onClick={() => handleCategoryClick("expectComment")}
+      >
+        기대평
+      </S.Category>
+      <S.Category
+        $isSelected={selectedCategory === "showPoster"}
+        onClick={() => handleCategoryClick("showPoster")}
+      >
+        공연 이미지
+      </S.Category>
+      <S.Category
+        $isSelected={selectedCategory === "productInfo"}
+        onClick={() => handleCategoryClick("productInfo")}
+      >
+        상품정보
+      </S.Category>
     </S.CategoryWrapper>
   );
 };
