89fe2d071d120e6839728303fc8c467f7aa96bbd
imhwarang <imhwarang4@naver.com>
2024-05-17 02:02:33 +0900

    Feat: 실시간 서칭 및 SearchList 구현

--- src/pages/Search/components/SearchList/SearchList.tsx
+++ src/pages/Search/components/SearchList/SearchList.tsx
@@ -1,5 +1,7 @@
 import useGetSearchResult from "../../../../hooks/useGetSearchResult.ts";
 import * as S from "./SearchList.styled.ts";
+import showImg from "../../../../assets/images/show.png";
+import { useState } from "react";
 
 const SearchList = ({ input }) => {
   const { searchResult } = useGetSearchResult();
@@ -8,12 +10,16 @@ const SearchList = ({ input }) => {
       item.title.toLowerCase().includes(input.toLowerCase()) ||
       item.location.toLowerCase().includes(input.toLowerCase())
   );
+
   return (
     <S.SearchListWrapper>
       {filteredResult.map((item, i) => (
         <S.SearchLi key={item.id}>
-          <S.LiImg src={item.image} />
-          <S.LiTitle>{item.title}</S.LiTitle>
+          <S.LiImg src={showImg} alt="showimg" />
+          <S.LiTextBox>
+            <S.LiLocation>{`[${item.location}] `}</S.LiLocation>
+            <S.LiTitle>{item.title}</S.LiTitle>
+          </S.LiTextBox>
         </S.SearchLi>
       ))}
     </S.SearchListWrapper>
