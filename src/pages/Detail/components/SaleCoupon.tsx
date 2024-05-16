53303419cbefa7f5ac8a3730aaedcb934a56a18d
namdaeun <nde40345@gmail.com>
2024-05-16 15:37:25 +0900

    Feat: 컴포넌트 뼈대 생성

---
+++ src/pages/Detail/components/SaleCoupon.tsx
@@ -0,0 +1,21 @@
+import { IcDownload } from "../../../assets/icons";
+import * as S from "./SaleCoupon.styled.ts";
+
+const SaleCoupon = () => {
+  return (
+    <S.CouponWrapper>
+      <S.CouponLayout>
+        <S.AvailableCouponTxt>
+          사용가능한 할인쿠폰
+          <S.AvailableCoupon> 2</S.AvailableCoupon>개
+        </S.AvailableCouponTxt>
+        <S.DownloadBtn>
+          다운
+          <IcDownload />
+        </S.DownloadBtn>
+      </S.CouponLayout>
+    </S.CouponWrapper>
+  );
+};
+
+export default SaleCoupon;
