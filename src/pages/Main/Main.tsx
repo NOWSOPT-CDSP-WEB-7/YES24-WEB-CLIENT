357067ae1a43b52ba78f95aebd0c114792406d0c
ocahs9 <piorag99@gmail.com>
2024-05-16 19:13:21 +0900

    Fix: 주석 제거

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,8 +3,6 @@ import MainCarousel from "./components/MainCarousel/MainCarousel";
 import Footer from "../../components/commons/Footer/Footer";
 import { useNavigate } from "react-router-dom";
 
-//import * as S from "./Main.styled.ts";
-
 const Main = () => {
   const navigate = useNavigate();
   const navigateSearch = () => {
