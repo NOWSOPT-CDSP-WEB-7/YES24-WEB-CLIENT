fb1a98616417488c074e86d4349e4977feda99e6
ocahs9 <piorag99@gmail.com>
2024-05-18 13:12:06 +0900

    Refactor: 카테고리목록 상수파일로 빼줌

--- src/pages/Main/components/MainCategory/MainCategory.tsx
+++ src/pages/Main/components/MainCategory/MainCategory.tsx
@@ -1,18 +1,10 @@
+import { CATEGORY } from "@constants/categoty";
 import * as S from "./MainCategory.styled";
 
 const MainCategory = () => {
-  const category: string[] = [
-    "콘서트",
-    "뮤지컬",
-    "연극",
-    "클래식/무용",
-    "전시/행사",
-    "가족/어린이",
-  ];
-
   return (
     <S.Category>
-      {category.map((text, idx) => (
+      {CATEGORY.map((text, idx) => (
         <S.CategoryCell key={`category-${idx}`}>{text}</S.CategoryCell>
       ))}
     </S.Category>
