e0b23bc4a50bbec729c4f75721d705a7d04500b3
ocahs9 <piorag99@gmail.com>
2024-05-18 22:51:02 +0900

    Feat: 티켓오픈컴포넌트 구현 완료 + IcMainBanner 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,11 +1,12 @@
+export { default as IcChevronrRight } from "./ic_chevron-right.svg?react";
 export { default as IcChevronUp } from "./ic_chevron_up.svg?react";
+export { default as IcDevider10px } from "./ic_devider_10px.svg?react";
 export { default as IcGlobalBtn } from "./ic_globalBtn.svg?react";
 export { default as IcHamburger } from "./ic_hamburger.svg?react";
 export { default as IcLine } from "./ic_line.svg?react";
 export { default as IcLogo } from "./ic_logo_svg.svg?react";
+export { default as IcMainBanner } from "./ic_main_banner.svg?react";
 export { default as IcMy } from "./ic_my.svg?react";
+export { default as IcOnlyBox } from "./ic_only_box.svg?react";
 export { default as IcSearch } from "./ic_search.svg?react";
 export { default as IcYes24Btn } from "./ic_yes24Btn.svg?react";
-export { default as IcOnlyBox } from "./ic_only_box.svg?react";
-export { default as IcChevronrRight } from "./ic_chevron-right.svg?react";
-export { default as IcDevider10px } from "./ic_devider_10px.svg?react";
