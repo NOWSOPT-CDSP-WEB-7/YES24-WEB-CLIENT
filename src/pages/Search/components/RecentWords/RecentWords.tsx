5e14c9567998ccb2b632c387f4b0e71757b25f3e
imhwarang <imhwarang4@naver.com>
2024-05-20 00:24:01 +0900

    Feat: 최근검색어 RecentWord 구현

--- src/pages/Search/components/RecentWords/RecentWords.tsx
+++ src/pages/Search/components/RecentWords/RecentWords.tsx
@@ -1,16 +1,23 @@
 import * as S from "./RecentWords.styled";
-import useGetRecentSearch from "../../../../hooks/useGetRecentSearch";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import RecentWord from "../RecentWord/RecentWord";
 
 const RecentWords = () => {
-  const { getRecentSearch } = useGetRecentSearch();
-  const [words, setWords] = useState([]);
-  useEffect(() => {
-    const getWords = getRecentSearch();
-    setWords(getWords);
-  }, []);
-  console.log(words);
-  return <div>RecentWords</div>;
+  const words = ["레베카", "울산", "서울"];
+  if (words.length === 0) {
+    return <div>최근 검색어가 없습니다.</div>;
+  }
+  return (
+    <S.Wrapper>
+      {words && (
+        <div>
+          {words.map((word, i) => {
+            return <RecentWord key={i} word={word} />;
+          })}
+        </div>
+      )}
+    </S.Wrapper>
+  );
 };
 
 export default RecentWords;
