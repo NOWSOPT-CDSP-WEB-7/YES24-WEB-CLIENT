f62f34cb0601e9f1e19f57602ebacd338c2e06f7
namdaeun <nde40345@gmail.com>
2024-05-20 19:47:55 +0900

    Feat: Info 컴포넌트 뼈대 생성

--- src/pages/Detail/components/Info/Info.styled.ts
+++ src/pages/Detail/components/Info/Info.styled.ts
@@ -4,6 +4,19 @@ export const InfoWrapper = styled.section`
   padding: 1.8rem 0 0 4.3rem;
 `;
 
-export const ShowTime = styled.section`
+export const TimeTitle = styled.h2`
   padding: 2rem 1rem;
+  ${({ theme }) => theme.fonts.title_15pt_Bold};
 `;
+
+export const Time = styled.span`
+  white-space: pre-line;
+`;
+
+export const NoticeTitle = styled.h3`
+  ${({ theme }) => theme.fonts.title_14pt_Bold};
+`;
+
+export const NoticeTime = styled.span``;
+
+export const ShowImg = styled.img``;
--- src/pages/Detail/components/Info/Info.tsx
+++ src/pages/Detail/components/Info/Info.tsx
@@ -1,9 +1,15 @@
+import { DETAIL_INFO } from "../../../../constants/detailInfo";
 import * as S from "./Info.styled";
 
 const Info = () => {
   return (
     <S.InfoWrapper>
-      <S.ShowTime></S.ShowTime>
+      <S.TimeTitle>공연시간</S.TimeTitle>
+      <S.Time>{DETAIL_INFO.time}</S.Time>
+      <S.NoticeTitle>알립니다</S.NoticeTitle>
+      <S.NoticeTime>{DETAIL_INFO.time}</S.NoticeTime>
+      <S.ShowImg src={DETAIL_INFO.showImg1} />
+      <S.ShowImg src={DETAIL_INFO.showImg2} />
     </S.InfoWrapper>
   );
 };
