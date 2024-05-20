f40b7b21991bf221729e327c1898f813d58714fd
namdaeun <nde40345@gmail.com>
2024-05-21 06:22:18 +0900

    Style: 스타일 적용

---
+++ src/pages/Detail/components/ExpectComment/ExpectComment.styled.ts
@@ -0,0 +1,119 @@
+import styled from "styled-components";
+
+export const PageWrapper = styled.div`
+  padding: 0 1rem;
+`;
+
+export const NoticeLayout = styled.section`
+  padding: 2rem 0;
+`;
+
+export const Notice = styled.h3`
+  padding-bottom: 0.8rem;
+  ${({ theme }) => theme.fonts.title_14pt_Bold};
+`;
+
+export const HighLight = styled.div`
+  color: ${({ theme }) => theme.colors.Tertiary_red};
+  ${({ theme }) => theme.fonts.sub_12pt};
+`;
+
+export const NoticeContent = styled.div`
+  color: ${({ theme }) => theme.colors.Text_01};
+  ${({ theme }) => theme.fonts.sub_12pt};
+`;
+
+export const BtnLayout = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.8rem 1rem;
+`;
+
+export const WriteBtn = styled.button`
+  width: 100%;
+  padding: 1.2rem 1rem;
+
+  color: ${({ theme }) => theme.colors.Text_03};
+
+  ${({ theme }) => theme.fonts.sub_14pt};
+  background-color: ${({ theme }) => theme.colors.Primary_Black};
+`;
+
+export const CommentLayout = styled.section``;
+
+export const CategoryBox = styled.section`
+  display: flex;
+  gap: 1rem;
+  padding-left: 1rem;
+`;
+
+export const Category = styled.span<{ $isActive: boolean }>`
+  padding: 1rem;
+
+  color: ${({ $isActive, theme }) => ($isActive ? theme.colors.Text_strong : theme.colors.Text_02)};
+  ${({ theme }) => theme.fonts.sub_11pt};
+`;
+
+export const CommentBox = styled.article`
+  padding: 1rem;
+
+  border-top: 1px solid ${({ theme }) => theme.colors.UI_02};
+`;
+
+export const ContentWithBtn = styled.div`
+  display: flex;
+  gap: 2rem;
+`;
+
+export const CommentContent = styled.span`
+  ${({ theme }) => theme.fonts.title_14pt_Bold};
+`;
+
+export const CommentInfoBox = styled.section`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+export const CommentInfo = styled.span`
+  padding: 0.8rem 0;
+
+  color: ${({ theme }) => theme.colors.Text_01};
+  ${({ theme }) => theme.fonts.sub_11pt};
+`;
+
+export const CommentDate = styled.div`
+  color: ${({ theme }) => theme.colors.Text_02};
+`;
+
+export const ReportBtn = styled.button`
+  display: flex;
+
+  color: ${({ theme }) => theme.colors.Text_01};
+  white-space: nowrap;
+  ${({ theme }) => theme.fonts.sub_11pt};
+`;
+
+export const BtnBox = styled.section`
+  display: flex;
+  gap: 0.4rem;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+
+  border-top: 1px solid ${({ theme }) => theme.colors.UI_02};
+`;
+
+export const MoreBtn = styled.button`
+  ${({ theme }) => theme.fonts.title_14pt_Bold};
+`;
+
+export const BannerFirst = styled.img`
+  width: 37.5rem;
+  height: 7rem;
+`;
+
+export const BannerSecond = styled.img`
+  width: 37.5rem;
+  height: 5.1rem;
+`;
