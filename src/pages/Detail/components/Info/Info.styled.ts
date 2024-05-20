5f35ede218fb5159e32e9e87b750deaa0c96074e
namdaeun <nde40345@gmail.com>
2024-05-21 04:05:43 +0900

    Style: 스타일 추가

--- src/pages/Detail/components/Info/Info.styled.ts
+++ src/pages/Detail/components/Info/Info.styled.ts
@@ -1,22 +1,71 @@
 import styled from "styled-components";
 
 export const InfoWrapper = styled.section`
-  padding: 1.8rem 0 0 4.3rem;
+  padding: 1.8rem 1rem 0;
+`;
+
+export const TimeLayout = styled.section`
+  padding: 2rem 0;
+
+  border-top: 1px solid ${({ theme }) => theme.colors.UI_02};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.UI_02};
 `;
 
 export const TimeTitle = styled.h2`
-  padding: 2rem 1rem;
   ${({ theme }) => theme.fonts.title_15pt_Bold};
 `;
 
 export const Time = styled.span`
+  color: ${({ theme }) => theme.colors.Text_01};
   white-space: pre-line;
+  ${({ theme }) => theme.fonts.sub_13pt_bold};
+`;
+
+export const NoticeLayout = styled.section`
+  padding: 2rem 0;
 `;
 
 export const NoticeTitle = styled.h3`
+  padding: 2rem 0 0.8rem;
   ${({ theme }) => theme.fonts.title_14pt_Bold};
 `;
 
-export const NoticeTime = styled.span``;
+export const NoticeTime = styled.span`
+  display: block;
+  padding-bottom: 1rem;
 
-export const ShowImg = styled.img``;
+  color: ${({ theme }) => theme.colors.Primary_orange};
+  white-space: pre-line;
+  ${({ theme }) => theme.fonts.sub_14pt};
+`;
+
+export const NoticeDetail = styled.span`
+  white-space: pre-line;
+  ${({ theme }) => theme.colors.Text_01};
+  ${({ theme }) => theme.fonts.sub_12pt};
+`;
+
+export const ImgLayout = styled.section`
+  padding: 1rem 0;
+`;
+
+export const ShowFirstImg = styled.img`
+  width: 35.5rem;
+  height: 41.6rem;
+  padding-bottom: 1rem;
+`;
+
+export const ShowSecondImg = styled.img`
+  width: 35.5rem;
+  height: 101.1rem;
+`;
+
+export const BannerFirstImg = styled.img`
+  width: 35.5rem;
+  height: 4.8rem;
+`;
+
+export const BannerSecondImg = styled.img`
+  width: 35.5rem;
+  height: 6.6rem;
+`;
--- src/pages/Detail/components/Info/Info.tsx
+++ src/pages/Detail/components/Info/Info.tsx
@@ -4,12 +4,21 @@ import * as S from "./Info.styled";
 const Info = () => {
   return (
     <S.InfoWrapper>
-      <S.TimeTitle>공연시간</S.TimeTitle>
-      <S.Time>{DETAIL_INFO.time}</S.Time>
-      <S.NoticeTitle>알립니다</S.NoticeTitle>
-      <S.NoticeTime>{DETAIL_INFO.time}</S.NoticeTime>
-      <S.ShowImg src={DETAIL_INFO.showImg1} />
-      <S.ShowImg src={DETAIL_INFO.showImg2} />
+      <S.TimeLayout>
+        <S.TimeTitle>공연시간</S.TimeTitle>
+        <S.Time>{DETAIL_INFO.time}</S.Time>
+      </S.TimeLayout>
+      <S.NoticeLayout>
+        <S.NoticeTitle>알립니다</S.NoticeTitle>
+        <S.NoticeTime>{DETAIL_INFO.time}</S.NoticeTime>
+        <S.NoticeDetail>{DETAIL_INFO.notice}</S.NoticeDetail>
+      </S.NoticeLayout>
+      <S.ImgLayout>
+        <S.ShowFirstImg src={DETAIL_INFO.showImg1} />
+        <S.ShowSecondImg src={DETAIL_INFO.showImg2} />
+      </S.ImgLayout>
+      <S.BannerFirstImg src={DETAIL_INFO.bannerImg1} />
+      <S.BannerSecondImg src={DETAIL_INFO.bannerImg2} />
     </S.InfoWrapper>
   );
 };
