ba910c8dc84d30b02955ca083b6dd11da41ed7c9
namdaeun <nde40345@gmail.com>
2024-05-21 06:55:45 +0900

    Feat: 뼈대와 스타일 적용

--- src/pages/Detail/components/ShowPoster/ShowPoster.tsx
+++ src/pages/Detail/components/ShowPoster/ShowPoster.tsx
@@ -1,5 +1,11 @@
+import * as S from "./ShowPoster.styled";
+
 const ShowPoster = () => {
-  return <div>ShowPoster</div>;
+  return (
+    <S.PosterWrapper>
+      <S.Poster src={"/src/assets/images/poster.png"} />
+    </S.PosterWrapper>
+  );
 };
 
 export default ShowPoster;
