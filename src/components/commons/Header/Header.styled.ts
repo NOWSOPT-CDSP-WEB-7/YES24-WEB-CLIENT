bc3152daf0c47b2e9c916b2b64fa5a83c87a50ff
namdaeun <nde40345@gmail.com>
2024-05-22 14:46:43 +0900

    Hotfix: Header 너비 고정

--- src/components/commons/Header/Header.styled.ts
+++ src/components/commons/Header/Header.styled.ts
@@ -4,7 +4,7 @@ export const HeaderWrapper = styled.header`
   position: fixed;
   z-index: 2;
   display: flex;
-  width: 100%;
+  width: 37.5rem;
   padding: 0.5rem 0 0.5rem 1rem;
 
   background-color: ${({ theme }) => theme.colors.UI_background};
