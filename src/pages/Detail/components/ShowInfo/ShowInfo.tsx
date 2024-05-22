a07ecc1b15d385b301fcb1c27f1869d15fba0d99
namdaeun <nde40345@gmail.com>
2024-05-22 23:50:43 +0900

    Feat: 데이터 가져와서 뿌리기

--- src/pages/Detail/components/ShowInfo/ShowInfo.tsx
+++ src/pages/Detail/components/ShowInfo/ShowInfo.tsx
@@ -1,47 +1,65 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { IcBtnShare, IcHeartFill } from "../../../../assets/icons";
 import { categories } from "../../../../constants/showDetailCategory";
-import { SHOW_DETAIL } from "../../../../constants/showDetailData";
 
 import * as S from "./ShowInfo.styled";
+import { ShowDetailPropTypes, fetchShowDetail } from "../../../../apis/Detail/fetchShowDetail";
 
 const ShowInfo = () => {
+  const { runShowId } = useParams();
+  const [showData, setShowData] = useState<ShowDetailPropTypes>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (runShowId) {
+        try {
+          const data = await fetchShowDetail(Number(runShowId));
+          if (data) {
+            setShowData(data);
+            console.log(data);
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+    fetchData();
+  }, [runShowId]);
+
   return (
     <>
-      {SHOW_DETAIL.data.map(
-        ({ runShowID, genre, title, period, filmRating, location, place, image, runtime }) => (
-          <S.ShowWrapper key={runShowID}>
-            <S.Title>
-              [{location}] {title}
-            </S.Title>
-            <S.ShowContentLayout>
-              <S.ShowImage src={image} alt="공연 포스터" />
-              <S.InfoWithBtnLayout>
-                <S.ShowInfoContainer>
-                  <S.CategoryBox>
-                    {categories.map((category) => (
-                      <S.Category>{category}</S.Category>
-                    ))}
-                  </S.CategoryBox>
-                  <S.InfoBox>
-                    <S.Info>{genre}</S.Info>
-                    <S.Info>{period}</S.Info>
-                    <S.Place>{place}</S.Place>
-                    <S.Info>{filmRating}세 이상 관람가</S.Info>
-                    <S.Info>{runtime}분 (인터미션: 20분 포함)</S.Info>
-                  </S.InfoBox>
-                </S.ShowInfoContainer>
-                <S.BtnLayout>
-                  <S.HeartBtnBox>
-                    <IcHeartFill />
-                    <S.HeartNum>48</S.HeartNum>
-                  </S.HeartBtnBox>
-                  <IcBtnShare />
-                </S.BtnLayout>
-              </S.InfoWithBtnLayout>
-            </S.ShowContentLayout>
-          </S.ShowWrapper>
-        )
-      )}
+      <S.ShowWrapper key={showData?.runShowID}>
+        <S.Title>
+          [{showData?.location}] {showData?.title}
+        </S.Title>
+        <S.ShowContentLayout>
+          <S.ShowImage src={showData?.imgUrl} alt="공연 포스터" />
+          <S.InfoWithBtnLayout>
+            <S.ShowInfoContainer>
+              <S.CategoryBox>
+                {categories.map((category, index) => (
+                  <S.Category key={index}>{category}</S.Category>
+                ))}
+              </S.CategoryBox>
+              <S.InfoBox>
+                <S.Info>{showData?.genre}</S.Info>
+                <S.Info>{showData?.period}</S.Info>
+                <S.Place>{showData?.place}</S.Place>
+                <S.Info>{showData?.filmRating}세 이상 관람가</S.Info>
+                <S.Info>{showData?.runTime}분 (인터미션: 20분 포함)</S.Info>
+              </S.InfoBox>
+            </S.ShowInfoContainer>
+            <S.BtnLayout>
+              <S.HeartBtnBox>
+                <IcHeartFill />
+                <S.HeartNum>{showData?.likeCount}</S.HeartNum>
+              </S.HeartBtnBox>
+              <IcBtnShare />
+            </S.BtnLayout>
+          </S.InfoWithBtnLayout>
+        </S.ShowContentLayout>
+      </S.ShowWrapper>
     </>
   );
 };
