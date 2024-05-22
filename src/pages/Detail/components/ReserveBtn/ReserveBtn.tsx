0b5e27196e9a516c63047461f26cef567c3c6551
namdaeun <nde40345@gmail.com>
2024-05-22 18:57:48 +0900

    Feat: 예매하기 버튼 누르면 POST

--- src/pages/Detail/components/ReserveBtn/ReserveBtn.tsx
+++ src/pages/Detail/components/ReserveBtn/ReserveBtn.tsx
@@ -1,9 +1,21 @@
 import * as S from "./ReserveBtn.styled";
+import { postBooking } from "../../../../apis/Detail/postBooking";
+// import { useParams } from "react-router-dom";
 
 const ReserveBtn = () => {
+  // const runShowId = useParams();
+
+  const postData = async () => {
+    try {
+      postBooking(Number(10));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <S.BtnWrapper>
-      <S.Button>예매하기</S.Button>
+      <S.Button onClick={postData}>예매하기</S.Button>
     </S.BtnWrapper>
   );
 };
