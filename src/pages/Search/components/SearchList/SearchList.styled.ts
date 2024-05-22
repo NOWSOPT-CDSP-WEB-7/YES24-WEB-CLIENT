ec904bfe0a3dc486e97c4b81f1073a53fff9850c
imhwarang <imhwarang4@naver.com>
2024-05-22 11:50:08 +0900

    Fix: z-index 수정

--- src/pages/Search/components/SearchList/SearchList.styled.ts
+++ src/pages/Search/components/SearchList/SearchList.styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const SearchListWrapper = styled.section`
   position: absolute;
-  z-index: 99;
+  z-index: 1;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
