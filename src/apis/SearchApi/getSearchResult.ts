a0dbd6219ea75cdf50dc04833963ed3dc87cc9df
imhwarang <imhwarang4@naver.com>
2024-05-22 16:50:22 +0900

    Feat: 검색어 localStorage에 저장 및 get api요청 로직 작성

---
+++ src/apis/SearchApi/getSearchResult.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const getSearchResult = async (input: string) => {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_BASE_URL}/runshow/search/?query=${input}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
