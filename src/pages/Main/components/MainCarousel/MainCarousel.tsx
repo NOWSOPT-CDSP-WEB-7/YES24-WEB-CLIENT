296c2788f19ff429bd0e427df9edb0483554440f
ocahs9 <piorag99@gmail.com>
2024-05-24 01:31:25 +0900

    Feat: 로딩중에는 loading 뜨게 만듬 - 짧은 찰나라서 거의 안보임

--- src/pages/Main/components/MainCarousel/MainCarousel.tsx
+++ src/pages/Main/components/MainCarousel/MainCarousel.tsx
@@ -10,6 +10,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import * as S from "./MainCarousel.styled";
 import "./swiperStyles.css";
 const MainCarousel = () => {
+  const [loading, setLoading] = useState(true);
   interface MainResponsObjPropTypes {
     id: number;
     title: string;
@@ -19,7 +20,6 @@ const MainCarousel = () => {
     place: string;
   }
 
-  //한번 다른 식으로도 사용해봄
   type MainResponsePropTypes = MainResponsObjPropTypes[];
   const [mainResponse, setMainResponse] = useState<MainResponsePropTypes>([]);
 
@@ -27,50 +27,55 @@ const MainCarousel = () => {
     const getData = async () => {
       const data = await fetchMain();
       setMainResponse(data);
+      setLoading(false);
     };
 
     getData();
   }, []);
   return (
     <>
-      <Swiper
-        modules={[Autoplay]}
-        spaceBetween={10}
-        slidesPerView={1.04}
-        centeredSlides={false}
-        loop={true}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
-        className={"mainSwiper"}
-      >
-        {mainResponse.map((item: MainResponsObjPropTypes, index: number) => (
-          <SwiperSlide key={item.id}>
-            <S.SwiperCard imgsrc={item.image}>
-              <S.GradationBox>
-                <S.ContentBox>
-                  <S.TextBox>
-                    <S.TitleAndSubTitle>
-                      <S.Title>
-                        <IcOnlyBox />
-                        <div>{item.title}</div>
-                      </S.Title>
-                      <S.SubTitle>
-                        <div>{item.subTitle}</div>
-                      </S.SubTitle>
-                    </S.TitleAndSubTitle>
-                    <S.PeriodAndPlace>{formatData(item.period, item.place)}</S.PeriodAndPlace>
-                  </S.TextBox>
-                  <S.Indicator>
-                    <S.CurrentIndex>
-                      {index + 1}
-                      <S.TotalIndex>/12</S.TotalIndex>
-                    </S.CurrentIndex>
-                  </S.Indicator>
-                </S.ContentBox>
-              </S.GradationBox>
-            </S.SwiperCard>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      {loading ? (
+        <h1>loading</h1>
+      ) : (
+        <Swiper
+          modules={[Autoplay]}
+          spaceBetween={10}
+          slidesPerView={1.04}
+          centeredSlides={false}
+          loop={true}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          className={"mainSwiper"}
+        >
+          {mainResponse.map((item: MainResponsObjPropTypes, index: number) => (
+            <SwiperSlide key={item.id}>
+              <S.SwiperCard imgsrc={item.image}>
+                <S.GradationBox>
+                  <S.ContentBox>
+                    <S.TextBox>
+                      <S.TitleAndSubTitle>
+                        <S.Title>
+                          <IcOnlyBox />
+                          <div>{item.title}</div>
+                        </S.Title>
+                        <S.SubTitle>
+                          <div>{item.subTitle}</div>
+                        </S.SubTitle>
+                      </S.TitleAndSubTitle>
+                      <S.PeriodAndPlace>{formatData(item.period, item.place)}</S.PeriodAndPlace>
+                    </S.TextBox>
+                    <S.Indicator>
+                      <S.CurrentIndex>
+                        {index + 1}
+                        <S.TotalIndex>/12</S.TotalIndex>
+                      </S.CurrentIndex>
+                    </S.Indicator>
+                  </S.ContentBox>
+                </S.GradationBox>
+              </S.SwiperCard>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
     </>
   );
 };
