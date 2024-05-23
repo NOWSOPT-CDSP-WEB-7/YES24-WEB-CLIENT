11ccb204f207a915984a9fbf62f856bd602a515c
imhwarang <imhwarang4@naver.com>
2024-05-23 20:35:09 +0900

    Feat: 클릭한 공연 localStorage에 저장 후 최근 검색 공연에서 불러오기

--- src/pages/Search/components/RecentShows/RecentShows.tsx
+++ src/pages/Search/components/RecentShows/RecentShows.tsx
@@ -1,10 +1,36 @@
 import DeleteAllBtn from "@components/commons/DeleteAllBtn/DeleteAllBtn";
 import SearchedShow from "../../../../components/commons/SearchedShow/SearchedShow";
+import { useEffect, useState } from "react";
+
+interface SearchResultPropTypes {
+  id: number;
+  title: string;
+  period: string;
+  location: string;
+  place: string;
+  genre: string;
+  image: string;
+}
 
 const RecentShows = () => {
+  const [recentShows, setRecentShows] = useState<SearchResultPropTypes[]>([]);
+  useEffect(() => {
+    const recentShowsArr = localStorage.getItem("recentShows");
+    if (recentShowsArr) {
+      setRecentShows(JSON.parse(recentShowsArr));
+    } else {
+      localStorage.setItem("recentShows", JSON.stringify([]));
+    }
+  }, []);
+  if (recentShows.length === 0) {
+    return <div>최근 검색 공연이 없습니다</div>;
+  }
+  console.log(recentShows);
   return (
     <>
-      <SearchedShow />
+      {recentShows.length !== 0 &&
+        recentShows.map((recentShow) => <SearchedShow show={recentShow} key={recentShow.id} />)}
+
       <DeleteAllBtn />
     </>
   );
